838b2fb88dc80e5f3afcc1c3c2e3630ba4157036
SilenceZhou <1063123603@qq.com>
2020-02-23 00:26:02 +0800

    Initial Commit

---
+++ SwiftUILearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILearning
+//
+//  Created by SilenceZhou on 2020/2/23.
+//  Copyright © 2020 SilenceZhou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
